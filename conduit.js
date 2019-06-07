c819e530a5f745c43147c82c22933540bc46dc53
Sam Johnson <sam@durosoft.com>
2019-06-07 14:40:09 -0400

    add conduit_setHTML method

--- conduit.js
+++ conduit.js
@@ -1,5 +1,9 @@
-// Conduit main bootstrap file
+// conduit main bootstrap file
 
 function conduit_setHTML(target, html) {
-
+  target.innerHTML = html;
+  var scripts = target.getElementsByTagName('script');
+  for(var i = 0; i < scripts.length; ++i) {
+    eval(scripts[i].innerHTML);
+  }
 }
