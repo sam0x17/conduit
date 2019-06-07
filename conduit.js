885d4e6527a53aa95fbf56999cdfe5a0b3e50419
Sam Johnson <sam@durosoft.com>
2019-06-07 19:12:11 -0400

    add conduit_ajax method

--- conduit.js
+++ conduit.js
@@ -4,6 +4,31 @@ function conduit_setHTML(target, html) {
   target.innerHTML = html;
   var scripts = target.getElementsByTagName('script');
   for(var i = 0; i < scripts.length; ++i) {
-    eval(scripts[i].innerHTML);
+    window.eval(scripts[i].innerHTML)
   }
+}
+
+function conduit_stringifyForm(data) {
+  var formItems = [];
+  for(var prop in data) {
+    if(!form.hasOwnProperty(prop)) continue;
+    formItems.push(encodeURIComponent(prop) + '=' + encodeURIComponent(data[prop]));
+  }
+  var url = '';
+  if(formItems.length > 0) {
+    url += '?' + formItems.join('&');
+  }
+  return url;
+}
+
+function conduit_ajax(method, url, form, successCallback, failureCallback) {
+  var xhr = new XMLHttpRequest();
+  url += conduit_stringifyForm(form);
+  xhr.onreadystatechange = function() {
+    if(this.readyState != 4) return;
+    if(this.status == 200) successCallback(this.responseText);
+    else failureCallback(this);
+  };
+  xhr.open(method, url, true);
+  xhr.send();
 }
