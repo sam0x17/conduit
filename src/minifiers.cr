4a5d724056190bde74c9d1e210f215f3677638b3
Sam Johnson <sam@durosoft.com>
2019-06-07 03:52:38 -0400

    make minifiers accessible from easy-to-use methods

--- src/minifiers.cr
+++ src/minifiers.cr
@@ -11,6 +11,8 @@ module Conduit::Minifiers
   CLEAN_CSS_PATH = "#{PKG_DIR}/#{PLATFORM}-packages/clean-css-#{PLATFORM}"
   HTML_MINIFIER_PATH = "#{PKG_DIR}/#{PLATFORM}-packages/html-minifier-#{PLATFORM}"
   UGLIFY_PATH = "#{PKG_DIR}/#{PLATFORM}-packages/uglify-js-#{PLATFORM}"
+  HTML_MINIFIER_OPTIONS = "--case-sensitive --conservative-collapse --minify-css=true --minify-js=true --use-short-doctype"
+  CLEAN_CSS_OPTIONS = "--skip-rebase"
 
   class MinifierStorage
     extend BakedFileSystem
@@ -24,7 +26,7 @@ module Conduit::Minifiers
   end
 
   def self.initialize
-    if !File.exists?(PKG_DIR) || Time.now - File.info(PKG_DIR).modification_time > 1.day
+    if !File.exists?(PKG_DIR) || Time.now - File.info(PKG_DIR).modification_time > 1.week
       FileUtils.mkdir_p(PKG_DIR)
       FileUtils.rm_rf("#{PKG_DIR}/*")
       File.write(PKG_DIR + "/packages.tar.gz", MinifierStorage.get("packages.tar.gz").gets_to_end)
@@ -35,7 +37,30 @@ module Conduit::Minifiers
       assert! File.exists?(UGLIFY_PATH)
     end
   end
+
+  def self.minify_css(path)
+    `#{CLEAN_CSS_PATH} #{CLEAN_CSS_OPTIONS} "#{path}"`
+  end
+
+  def self.minify_html(path)
+    `#{HTML_MINIFIER_PATH} #{HTML_MINIFIER_OPTIONS} "#{path}"`
+  end
+
+  def self.minify_js(path)
+    `#{UGLIFY_PATH} "#{path}"`
+  end
+end
+
+def minify_js(path)
+  Conduit::Minifiers.minify_js(path)
+end
+
+def minify_css(path)
+  Conduit::Minifiers.minify_css(path)
+end
+
+def minify_html(path)
+  Conduit::Minifiers.minify_html(path)
 end
 
 Conduit::Minifiers.initialize
-puts "hello world"
