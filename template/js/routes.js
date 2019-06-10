a259e2838813533afdc96f9e83869ec352c404d3
Sam Johnson <sam@durosoft.com>
2019-06-10 19:45:16 -0400

    add documentation for routes.js file

--- template/js/routes.js
+++ template/js/routes.js
@@ -1,3 +1,14 @@
-
-conduit.route('/widgets/:id/show', 'widgets', 'A Widget');
-conduit.route('/users/:id/edit', 'users', 'A User');
+// add your routes here
+// syntax: conduit.route(url_format, view_path, page_title, condition (optional function returning a boolean));
+// routes should be listed in order of decreasing precedence
+//
+// e.g.:
+//
+// conduit.route('/widgets/:id/show', 'widgets', 'A Widget');
+// conduit.route('/users/:id/edit', 'some/path/users', 'A User');
+//
+// conduit.route('/blogs/#article_name', 'blog/article', 'Blog Article', function(urlVariables) {
+//   console.log('article name: ', urlVariables.article_name);
+//   return urlVariables.startsWith('Tutorial: ');
+// });
+//
