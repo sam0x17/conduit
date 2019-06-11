bf9654e85871b606aac2c49068fc30c7e4e94903
Sam Johnson <sam@durosoft.com>
2019-06-11 09:28:33 -0400

    support view nesting / partials via {{view name}}

--- src/compiler.cr
+++ src/compiler.cr
@@ -1,4 +1,13 @@
 
+class String
+  def gsub(other : Hash(String, String))
+    ret = self.clone
+    other.each do |other_k, other_v|
+      ret = ret.gsub(other_k, other_v)
+    end
+    ret
+  end
+end
 
 module Conduit
   def self.compile_views(test_mode=false)
@@ -6,18 +15,33 @@ module Conduit
     require_directory! "./views"
     require_file! "./views/router.html"
     router_html = File.read("./views/router.html")
-    views_str = "{\n"
+    view_replace_hash = Hash(String, String).new
+    views = Hash(String, String).new
+    view_names = Hash(String, String).new
     find_files("./views") do |path|
       next if path == "./views/router.html"
+      name = path[8..]
+      name = name[..(name.size - 6)] if name.ends_with?(".html")
       if test_mode
         view = File.read(path)
       else
         view = minify_html(path)
       end
-      view = Base64.strict_encode(view)
-      path = path[8..]
-      path = path[..(path.size - 6)] if path.ends_with?(".html")
-      views_str += "\"#{path}\":\"#{view}\",\n"
+      view_replace_hash["{{#{name}}}"] = view
+      views[name] = view
+    end
+    2.times do
+      view_replace_hash.each do |name, contents|
+        view_replace_hash[name] = contents.gsub(view_replace_hash)
+      end
+    end
+    views.each do |name, contents|
+      views[name] = contents.gsub(view_replace_hash)
+    end
+    views_str = "{\n"
+    views.each do |name, contents|
+      contents = Base64.strict_encode(contents)
+      views_str += "\"#{name}\":\"#{contents}\",\n"
     end
     views_str = views_str[..(views_str.size - 3)] + "\n}"
     router_html = router_html.gsub("{{version}}", version)
