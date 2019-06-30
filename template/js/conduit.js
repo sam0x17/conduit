f87f3ef92c9c74c1377e9bcce1e335cf9bffde02
Sam Johnson <sam@durosoft.com>
2019-06-30 01:47:04 -0400

    only scroll to top when not a refresh

--- template/js/conduit.js
+++ template/js/conduit.js
@@ -177,7 +177,7 @@ function isFunction(v) { return !!(v instanceof Function); }
       if(virtualNavigation) history.pushState(null, title, currentPath);
       setHTML(document.body, html);
       triggerOnload();
-      window.scrollTo(0, 0); 
+      if(window.performance && window.performance.navigation.type != 1) window.scrollTo(0, 0); 
       if(conduit.onNavigate) conduit.onNavigate();
     }
   }
