cf37816ab93adba127a3d6a80c61a3deb8415f85
Sam Johnson <sam@durosoft.com>
2019-08-06 05:12:07 -0400

    fix typo

--- template/js/conduit.js
+++ template/js/conduit.js
@@ -178,7 +178,7 @@ function isFunction(v) { return !!(v instanceof Function); }
       if(virtualNavigation) history.pushState(null, title, currentPath);
       setHTML(document.body, html);
       triggerOnload();
-      window.scrollTo(0, 0); 
+      window.scrollTo(0, 0);
       if(conduit.onNavigate) conduit.onNavigate();
     }
   }
